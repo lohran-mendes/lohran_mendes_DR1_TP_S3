eb1c72e5c18530048edd6838ca186ee2310b4f22
Lohran Mendes <lohranfm@gmail.com>
2025-10-27 22:10:43 -0300

    feat: add App component structure and styling with navigation links

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,22 @@ import './App.css'
 function App() {
 
   return (
-    <>
-    </>
+    <div className="AppComponent">
+    <h1>Ao rodar o projeto é possível verificar as questões adicionando os caminhos na url de cada rota que é mencionada no arquivo main.jsx, ou clicando nos links abaixo:</h1>
+    <ul>
+      <li><a href="/question_01">Questão 01</a></li>
+      <li><a href="/question_02">Questão 02</a></li>
+      <li><a href="/question_03">Questão 03</a></li>
+      <li><a href="/question_04">Questão 04</a></li>
+      <li><a href="/question_05">Questão 05</a></li>
+      <li><a href="/question_06">Questão 06</a></li>
+      <li><a href="/question_07">Questão 07</a></li>
+      <li><a href="/question_08_09">Questões 08 e 09 </a></li>
+      <li><a href="/question_10_11">Questões 10 e 11 </a></li>
+      <li><a href="/question_12_13">Questões 12 e 13 </a></li>
+      <li><a href="/question_14_15_16">Questões 14, 15 e 16 </a></li>
+    </ul>
+    </div>
   )
 }
 
